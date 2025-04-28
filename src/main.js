3ccf3e35317b5acec235cef8db1b5d702651f938
DanielFelipeFlorezCubides <186082651+DanielFelipeFlorezCubides@users.noreply.github.com>
2025-04-28 12:58:44 -0500

    feat: :sparkles: Page design done

---
+++ src/main.js
@@ -0,0 +1,12 @@
+let nombre
+let ingredientes = []
+let instrucciones
+let tiempo
+let n_porciones
+let categoria = []
+let dificultad = []
+
+document.addEventListener("DOMContentLoaded", async () => {
+
+})
+
